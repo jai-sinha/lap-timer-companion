9feeabed3c3cd72fd601ded0662794c93d0a57f7
jsinha <jaisinha219@gmail.com>
2025-09-16 15:31:49 -0700

    Initial Commit

---
+++ LapTimerCompanion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LapTimerCompanion
+//
+//  Created by Jai Sinha on 9/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LapTimerCompanion/LapTimerCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LapTimerCompanionApp.swift
+//  LapTimerCompanion
+//
+//  Created by Jai Sinha on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct LapTimerCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
